ab244cfaedc7beeec5f215b6fafc3fb61865a960
hrx <hrx_ses@163.com>
2026-08-09 23:54:02 +0800

    commit rest of lab and modify answers-syscall.txt to make the answer clearer

--- user/attack.c
+++ user/attack.c
@@ -6,7 +6,24 @@
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
+  int size = 4096;  //  页大小
+  char* mem = sbrk(size);   //通过sbrk分配内存
 
-  exit(1);
+  if (mem == (char*)-1) {   //分配失败的情况
+    exit(1);
+  }
+
+  //扫描并试图找寻
+  //出现This may help.内容，锁定其首地址偏移16的位置
+  int i = 0;
+  while (i < size) {
+    if (mem[i] == 'T') {
+      if (!strcmp(&mem[i], "This may help.")&&i+16<size) {
+        printf("%s", &mem[i + 16]);
+      }
+    }
+    i++;
+  }
+
+  exit(0);
 }
